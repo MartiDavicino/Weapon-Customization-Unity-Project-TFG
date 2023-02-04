5bcefad6ed034a4b1c6576d0665f7d1ab8832e2e
MartiDavicino <60965161+MartiDavicino@users.noreply.github.com>
2023-02-04 12:20:52 +0100

    Sockets added and reimported assets

--- Modular Weapon System/Assets/WeaponGenerator.cs
+++ Modular Weapon System/Assets/WeaponGenerator.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class WeaponGenerator : MonoBehaviour
 {
+    GameObject previousWeapon = null;
+
     public List<GameObject> bodyParts;
 
     public List<GameObject> stockParts;
@@ -29,7 +31,7 @@ public class WeaponGenerator : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.Space))
         {
-            //DestroyCurrentWeapon();
+            DestroyCurrentWeapon();
             GenerateNewWeapon();
         }
     }
@@ -37,23 +39,41 @@ public class WeaponGenerator : MonoBehaviour
     private void GenerateNewWeapon()
     {
         Debug.Log("Generating new weapon");
-        GameObject weapon = new GameObject();
-        List<GameObject> weaponParts = new List<GameObject>();
+        //GameObject weapon = new GameObject();
+        //List<GameObject> weaponParts = new List<GameObject>();
 
         //Body
-        GameObject body = GetRandomPart(bodyParts);
-        //Instantiate(body,Vector3.zero,Quaternion.identity);
-        Instantiate(body);
-        weaponParts.Add(body);
+        GameObject randomBody = GetRandomPart(bodyParts);
+        GameObject instantiatedBody=Instantiate(randomBody);
+        Weapon weaponbody=instantiatedBody.GetComponent<Weapon>();
 
-        GameObject grip = GetRandomPart(gripParts);
-        Instantiate(grip);
-        weaponParts.Add(grip);
+        SpawnPart(stockParts, weaponbody.stockSocket);
+        SpawnPart(gripParts, weaponbody.gripSocket);
+        SpawnPart(magazineParts, weaponbody.magazineSocket);
+        SpawnPart(handguardParts, weaponbody.handguardSocket);
+        SpawnPart(scopeParts, weaponbody.scopeSocket);
+
+        //SpawnPart(barrelParts, );
+
+        //SpawnPart(muzzleParts, );
+
+        previousWeapon = instantiatedBody;
     }
 
     private void DestroyCurrentWeapon()
     {
-        throw new NotImplementedException();
+        if(previousWeapon != null)
+        {
+            Destroy(previousWeapon);
+        }
+    }
+
+
+    void SpawnPart(List<GameObject> parts,Transform socket)
+    {
+        GameObject randomPart=GetRandomPart(parts);
+        GameObject instantiatedPart=Instantiate(randomPart,socket.position,socket.rotation);
+        instantiatedPart.transform.parent = socket;
     }
 
     GameObject GetRandomPart(List <GameObject> partsList)
